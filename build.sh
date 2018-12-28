e3f4d7c6ec32d5ef5e0216d1dc967b0596f91470
Roger de la Fuente <rdelafuente@atlassian.com>
2018-12-28 18:31:13 +1100

    Pull before pushing

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -xe
 VERSION=${VERSION:-$(yarn info cypress version --json | jq -r .data)}
+docker pull rdelafuente/cypress:${VERSION} || echo "building from scratch"
+
 docker build . -t rdelafuente/cypress:${VERSION} --build-arg VERSION=${VERSION}
+
+docker push rdelafuente/cypress:${VERSION}
 docker tag rdelafuente/cypress:${VERSION} rdelafuente/cypress:latest
-docker push rdelafuente/cypress:${VERSION} rdelafuente/cypress:latest
+docker push rdelafuente/cypress:latest
